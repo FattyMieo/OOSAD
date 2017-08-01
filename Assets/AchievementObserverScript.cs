19ac02a521e3908d6cbf6a71a2cc96850c98f61f
FattyMieo <fong2009@live.com.my>
2017-08-01 15:01:22 +0800

    Achievement System

---
+++ Assets/AchievementObserverScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementObserverScript : MonoBehaviour 
+{
+	public AchievementType myType;
+	public string achievementName;
+	public Text achievementTitleText;
+	public Image achievementBar;
+
+	public int achievementMaxCount = 1;
+	public int achievementCount = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		AchievementSubjectScript.Instance.SubscribeObserver(this);
+		achievementTitleText.text = achievementName;
+		achievementBar.fillAmount = achievementCount/achievementMaxCount;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void Notify(AchievementType type)
+	{
+		//! How should i react to this notification?
+		if(myType == type)
+		{
+			achievementCount++;
+			achievementBar.fillAmount = (float)achievementCount/(float)achievementMaxCount;
+			if(achievementCount >= achievementMaxCount)
+			{
+				AchievementSubjectScript.Instance.UnSubscribeObserver(this);
+			}
+		}
+	}
+}
---
+++ Assets/AchievementSubjectScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum AchievementType
+{
+	ACHIEVEMENT_COUNT = 0,
+	ACHIEVEMENT_GOAL,
+	ACHIEVEMENT_LEVEL1,
+	ACHIEVEMENT_LEVEL2,
+	ACHIEVEMENT_LEVEL3,
+}
+
+public class AchievementSubjectScript : MonoBehaviour 
+{
+	public static AchievementSubjectScript Instance;
+
+	List<AchievementObserverScript> observerList = new List<AchievementObserverScript>();
+
+	public void Awake()
+	{
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Notify(AchievementType.ACHIEVEMENT_COUNT);
+		}
+	}
+
+	public void SubscribeObserver(AchievementObserverScript observerScript)
+	{
+		observerList.Add(observerScript);
+	}
+
+	public void UnSubscribeObserver(AchievementObserverScript observerScript)
+	{
+		observerList.Remove(observerScript);
+	}
+
+	public void Notify(AchievementType type)
+	{
+		for(int i = 0; i < observerList.Count; i++)
+		{
+			observerList[i].Notify(type);
+		}
+	}
+}
