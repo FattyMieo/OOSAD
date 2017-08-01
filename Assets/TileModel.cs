6b0f2e2320afab02ad4321f9465965582a4197c3
FattyMieo <fong2009@live.com.my>
2017-08-01 15:02:40 +0800

    Tile Generator with Flyweight

---
+++ Assets/TileModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileModel : MonoBehaviour 
+{
+	
+	public List <Sprite> tileSpriteList = new List<Sprite>();
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/TileScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType
+{
+	SAND,
+	BRICK,
+	WOOD,
+	NONE,
+}
+
+public class TileScript : MonoBehaviour 
+{
+	public TileModel tileModelScript;
+	public TileType myType;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		GetComponent<SpriteRenderer>().sprite=tileModelScript.tileSpriteList [(int)myType];
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
